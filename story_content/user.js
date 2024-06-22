d07792e202cbbc4b988265f4ea8ec7835d865b66
Daniqpl <danielle.walsh@queenslibrary.org>
2024-06-22 19:49:54 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ story_content/user.js
@@ -0,0 +1,19 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=month + "/" + day + "/" + year
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
+};
